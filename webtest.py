c79c82fccb0017d7c7a3678437c19012f5415e52
Arya Chandrasekharan <aryagchan@gmail.com>
2025-04-13 13:12:45 +0100

    testing 1.0.0

--- webtest.py
+++ webtest.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 app = Flask(__name__)
+name = "Tim"
+@app.route("/")
+def home():
+    return "Hello"
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
+@app.route("/admin")
+def admin():
+    return redirect(url_for("user", name="Admin!"))
+
 if __name__ == "__main__":
     app.run()
-
-@app.route('/')
-def home():
-    return "Hello! This is an initial test <h1>HELLO</h1>"
