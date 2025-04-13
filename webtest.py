366ac8d2df1bdd0c386f44bee1adc2b9eb335708
Arya Chandrasekharan <aryagchan@gmail.com>
2025-04-13 16:30:02 +0100

    1.0.3 redirect and working CID form

--- webtest.py
+++ webtest.py
@@ -1,15 +1,25 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
-name = "Tim"
+name = "Bob"
 @app.route("/")
 def home():
     return render_template("index2.html")
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
+@app.route("/SOPs")
+def SOPs():
+    return redirect("https://clevelandcenter.org/downloads")
 @app.route("/admin")
 def admin():
-    return redirect(url_for("user", name="Admin!"))
-
+    return redirect(url_for("usern", name="Admin!"))
+@app.route("/login",methods=["POST","GET"])
+def login():
+    if request.method == "POST": 
+        user = request.form.get("nm")
+        return redirect(url_for("user",abc=user))
+    else:
+        return render_template("login.html")
+    
+@app.route("/<abc>",methods=["POST","GET"])
+def user(abc):
+    return f"welcome to the site"
 if __name__ == "__main__":
     app.run()
