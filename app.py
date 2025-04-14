dcbddb8fab5fd6015f66cefca71537cbb29e078f
Arya Chandrasekharan <aryagchan@gmail.com>
2025-04-14 14:22:06 +0100

    1.0.9 use datis to determine correct flow

--- app.py
+++ app.py
@@ -1,7 +1,51 @@
-from flask import Flask, redirect, url_for, render_template, request, jsonify
+from flask import Flask, redirect, url_for, render_template, request, jsonify, session, json
+import requests
+import re
 import sqlite3
 from datetime import timedelta
 app = Flask(__name__)
+
+RUNWAY_FLOW_MAP = {
+    "DTW": {
+        "SOUTH": ["21","22"],
+        "NORTH": ["3","4"],
+        "WEST": ["27"]
+    },
+    # Add more airports if needed
+}
+def get_flow(airport_code):
+    airport_code = airport_code.upper()
+    if airport_code not in RUNWAY_FLOW_MAP:
+        return None
+
+    try:
+        aptIcao = "K" + airport_code
+        datis_url = f"https://datis.clowd.io/api/{aptIcao}"
+        response = requests.get(datis_url)
+        
+        if response.status_code != 200:
+            return None
+        
+        atis_data = response.json()
+        atis_text = atis_data[1]
+        
+        # Clean up the ATIS text to help matching
+        atis_datis = atis_text['datis']
+
+        # Look for any runway in the ATIS text and return the matching flow
+        flow_config = RUNWAY_FLOW_MAP[airport_code]
+        for flow_direction, runways in flow_config.items():
+            for rwy in runways:
+                # Match like "DEP RWY 21L" or "DEPARTURE RUNWAY 21L"
+                if re.search(rf"DEPG RWY {rwy}[LRC]?", atis_datis):
+                    return flow_direction.upper()
+        print(f"No matching flow found for {airport_code}")
+
+        return None
+    except Exception as e:
+        print(f"Flow detection error for {airport_code}: {e}")
+        return None
+
 
 @app.route("/")
 def home():
@@ -55,9 +99,18 @@ def search():
     conn.close()
 
     for row in rows:
+        CurrFlow = ''
+        isActive = False
         route_origin = row[0]
         route_notes = row[4]
 
+        flow = ''
+        if destination in RUNWAY_FLOW_MAP:
+            CurrFlow = get_flow(destination)
+            if CurrFlow.upper() in route_notes.upper():
+                isActive = True
+            else:
+                isActive = False
         if origin and origin in route_notes:
             route_origin = origin
         
@@ -68,9 +121,16 @@ def search():
             'route': row[2],
             'altitude': row[3],
             'notes': route_notes,
+            'flow':CurrFlow or '',
+            'isActive':isActive 
         })
+    
     searched = True
     return render_template("search.html", routes=routes, searched=searched)    
+
+
+
+
 
 if __name__ == "__main__":
     app.run()
